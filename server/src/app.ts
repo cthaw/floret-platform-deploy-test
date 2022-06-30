d4aef0ed9d794809e3b2d470067642887b47dc3b
Chris <chris@tryfloret.com>
2022-06-30 12:53:45 -0400

    Created mechanism for pushing server runtime environment variables to client

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,8 +2,18 @@ import express from 'express';
 import history from 'connect-history-api-fallback';
 import compression from 'compression';
 import cors from 'cors';
+import fs from 'fs';
 import { CLIENT_DIR } from './constants';
 import routes from './routes';
+import {
+  PLATFORM_SERVER_BASE_URL,
+  PLATFORM_SERVER_BASE_URL_REGEX,
+  AUTH0_DOMAIN,
+  AUTH0_DOMAIN_REGEX,
+  AUTH0_CLIENT_ID,
+  AUTH0_CLIENT_ID_REGEX,
+  APP_JS_REGEX,
+} from './config';
 
 const app = express();
 
@@ -21,6 +31,19 @@ app.use(history({
     },
   ],
 }));
+app.get('/js/:filename', (req: express.Request, res: express.Response) => {
+  fs.readFile(CLIENT_DIR + req.url, 'utf-8', (error, data) => {
+    if (APP_JS_REGEX.test(req.url)) {
+      res.end(
+        data.replace(PLATFORM_SERVER_BASE_URL_REGEX, PLATFORM_SERVER_BASE_URL)
+          .replace(AUTH0_DOMAIN_REGEX, AUTH0_DOMAIN)
+          .replace(AUTH0_CLIENT_ID_REGEX, AUTH0_CLIENT_ID),
+      );
+    } else {
+      res.end(data);
+    }
+  });
+});
 app.use(express.static(CLIENT_DIR));
 app.use(routes);
 
